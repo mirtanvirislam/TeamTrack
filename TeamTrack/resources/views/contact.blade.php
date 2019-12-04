6b23b5c76b5f7bc8c3e42180340fb81fcbf0a51e
Mir Tanvir <tanvir98.mt@gmail.com>
2019-12-05 01:46:01 +0600

    Merge commit

--- TeamTrack/resources/views/contact.blade.php
+++ TeamTrack/resources/views/contact.blade.php
@@ -6,7 +6,7 @@
     <div class="container p-4">
         <div class="row justify-content-center card p-4">
             <div class="col-md-8 p-4">
-            <h1>Contact Teamtrack</h1>
+            <h1>Contact Teamtrack.</h1>
             <hr>
             <h5>Email : teamtrack@gmail.com</h5>
             <h5>Phone : ++880837234
