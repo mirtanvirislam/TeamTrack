22a4a0ffa0393a3b04b370d4936419dba2d55041
Mir Tanvir <tanvir98.mt@gmail.com>
2019-12-14 19:54:40 +0600

    Graph demo

--- TeamTrack/resources/views/about.blade.php
+++ TeamTrack/resources/views/about.blade.php
@@ -2,6 +2,7 @@
 
 @include('layouts.cssimports')
 <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.3/Chart.bundle.js"></script>
+<script src="https://www.chartjs.org/samples/latest/utils.js"></script>
 <body>
     <div class="container p-4">
         <div class="row justify-content-center card p-4">
@@ -12,46 +13,57 @@
                     Make work life simpler, more pleasant and more productive.
                 </h5>
                 
-<canvas id="myChart" width="400" height="400"></canvas>
-<script>
-var ctx = document.getElementById('myChart').getContext('2d');
-var myChart = new Chart(ctx, {
-    type: 'bar',
-    data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-        datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 30, 5, 2, 3],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 102, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-        }]
-    },
-    options: {
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero: true
-                }
-            }]
-        }
-    }
-});
-</script>
+                <canvas id="canvas" width="400" height="200"></canvas>
+
+                <script>
+                    var barChartData = {
+                        labels: ['Sprint 1', 'Sprint 2', 'Sprint 3', 'Sprint 4', 'Sprint 5', 'Sprint 6', 'Sprint 7'],
+                        datasets: [{
+                            label: 'Completed Tasks',
+                            backgroundColor: window.chartColors.blue,
+                            data: [4, 3, 2, 0, 0, 0,0]
+                        }, {
+                            label: 'Overdue Tasks',
+                            backgroundColor: window.chartColors.red,
+                            data: [
+                                0,0,1,0,0,0,0
+                            ]
+                        },{
+                            label: 'Scheduled Tasks',
+                            backgroundColor: window.chartColors.grey,
+                            data: [
+                                0,0,0,3,4,3,4
+                            ]
+                        }]
+
+                    };
+                    window.onload = function() {
+                        var ctx = document.getElementById('canvas').getContext('2d');
+                        window.myBar = new Chart(ctx, {
+                            type: 'bar',
+                            data: barChartData,
+                            options: {
+                                title: {
+                                    display: true,
+                                    
+                                },
+                                tooltips: {
+                                    mode: 'index',
+                                    intersect: false
+                                },
+                                responsive: true,
+                                scales: {
+                                    xAxes: [{
+                                        stacked: true,
+                                    }],
+                                    yAxes: [{
+                                        stacked: true
+                                    }]
+                                }
+                            }
+                        });
+                    };
+                </script>
 
             </div>
         </div>
